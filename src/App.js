1bd312478bc4eaa3df02f6d1cf45c403d08eb4f4
Ravi Nivangune <ravinivangune13@gmail.com>
2019-12-15 15:24:57 +0530

    Added css changes

--- src/App.js
+++ src/App.js
@@ -1,91 +1,99 @@
 import React, { Component } from "react";
 import "./App.css";
-import axios from 'axios'
-import ReactPlayer from 'react-player'
-
+import axios from "axios";
+import ReactPlayer from "react-player";
 
 class App extends Component {
   constructor(props) {
-    super(props)
+    super(props);
 
     this.state = {
-      userName: 'Ravi',
+      userName: "coderavi13",
       userList: [],
       showDetail: false,
-      userObject: {},
-    }
-
+      userObject: {}
+    };
   }
 
-  handleChange = (event) => {
+  handleChange = event => {
     this.setState({
-      userName: event.target.value,
-    })
-  }
+      userName: event.target.value
+    });
+  };
 
-  getData = (data) => {
+  getData = data => {
+    axios
+      .get("https://api.github.com/search/users", {
+        params: { q: data }
+      })
+      .then(apiResponse => {
+        console.log("ApiResponse", apiResponse.data.items);
+        this.setState({
+          showDetail: false,
+          userList: apiResponse.data.items
+        });
+      })
+      .catch(error => console.log("Error", error));
+  };
 
-    console.log("data", data)
-
-    axios.get('https://api.github.com/search/users', {
-      params: { q: data }
-    })
-      .then(
-        (apiResponse) => {
-          console.log("ApiResponse", apiResponse.data.items)
-          this.setState({
-            showDetail: false,
-            userList: apiResponse.data.items,
-          })
-        }
-      )
-      .catch(
-        error => console.log("Error", error)
-      )
-  }
-
-  displayUserData = (userData) => {
+  displayUserData = userData => {
     this.setState({
       showDetail: true,
       userObject: userData
-    })
-
-  }
+    });
+  };
 
   render() {
     return (
       <React.Fragment>
+        <label className="label">Enter username to search on github</label>
+        <input
+          value={this.state.userName}
+          type="text"
+          onChange={this.handleChange}
+        />
+        <button
+          className="button"
+          onClick={() => this.getData(this.state.userName)}
+        >
+          Click to Search
+        </button>
+        <ReactPlayer
+          url="https://www.youtube.com/watch?v=UX2kf-XngE0"
+          playing={false}
+          controls
+        />
 
-        <label className='label'>Enter username to search on github</label>
-        <input value={this.state.userName} type='text' onChange={this.handleChange} />
-        <button className='button' onClick={() => this.getData(this.state.userName)}>Click to Search</button>
-        <ReactPlayer url='https://www.youtube.com/watch?v=UX2kf-XngE0' playing={false} controls />
-        {!this.state.showDetail && <ul>
+        <div className="left">
           {
-            this.state.userList.map(
-              (user, index) => {
-                return <li key={index} onClick={() => this.displayUserData(user)} style={{ cursor: 'pointer' }}> {user.login}</li>
-              }
-            )
-          } </ul>}
+            <ul>
+              {this.state.userList.map((user, index) => {
+                return (
+                  <li
+                    key={index}
+                    onClick={() => this.displayUserData(user)}
+                    style={{ cursor: "pointer" }}
+                  >
+                    {user.login}
+                  </li>
+                );
+              })}
+            </ul>
+          }
+        </div>
 
-        {this.state.showDetail &&
-          <div>
-
-            <div>Score : {this.state.userObject.score}</div>
-            <div>id : {this.state.userObject.id}</div>
-            <img src={this.state.userObject.avatar_url} />
-
-
-          </div>
-
-        }
-
+        <div className="right">
+          {this.state.showDetail && (
+            <div>
+              <label>Score :</label> <div> {this.state.userObject.score}</div>
+              <label>Id :</label><div> {this.state.userObject.id}</div>
+              <img src={this.state.userObject.avatar_url} />
+            </div>
+          )}
+        </div>
       </React.Fragment>
-
     );
   }
-
 }
 
 export default App;
